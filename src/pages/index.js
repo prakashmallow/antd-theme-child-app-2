a37aafdea1223cfdd2c09cd1f974c84eaf80def7
pcc493_ftr <prakashks9562@gmail.com>
2024-08-23 08:50:13 +0530

    Temporary fix for antd theme issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Divider, Row, Col, Button } from 'antd';
+import { Divider, Row, Col, Button, ConfigProvider } from 'antd';
 import { useRouter } from "next/router";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
@@ -8,7 +8,7 @@ import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ theme = {} }) {
   const router = useRouter();
   return (
     <>
@@ -18,20 +18,22 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div>
-          <h3>About us</h3>
-          <Divider />
-          <Row>
-            <Col span={6}>
-              <Button type="primary" onClick={() => router.push('/services')}>Go to Services page</Button>
-            </Col>
-            <Col span={6}>
-              <Button onClick={() => router.push('/')}>Back to Home page</Button>
-            </Col>
-          </Row>
-        </div>
-      </main>
+      <ConfigProvider theme={theme}>
+        <main>
+          <div>
+            <h3>About us</h3>
+            <Divider />
+            <Row>
+              <Col span={6}>
+                <Button type="primary" onClick={() => router.push('/services')}>Go to Services page</Button>
+              </Col>
+              <Col span={6}>
+                <Button onClick={() => router.push('/')}>Back to Home page</Button>
+              </Col>
+            </Row>
+          </div>
+        </main>
+      </ConfigProvider>
     </>
   );
 }
